62495a6c8b6b217e149b72d69d5a625727988ad1
myungwan-git <nsubway1070635@gmail.com>
2021-11-12 14:37:11 +0900

    20211112 company work - html/css

--- main4.js
+++ main4.js
@@ -14,6 +14,7 @@ navInner.addEventListener('mouseleave', () => {
 * mouse over on pc menu -> display depth1 menu border-bottom red
 */
 const depth1 = document.querySelectorAll('.nav__depth1');
+const depth2 = document.querySelectorAll('.nav__depth2');
 depth1.forEach(function(element) {
   element.addEventListener('mouseover', () => {
     const depth1Parent = element.closest('li');
@@ -22,6 +23,16 @@ depth1.forEach(function(element) {
   element.addEventListener('mouseleave', () => {
     const depth1Parent = element.closest('li');
     depth1Parent.classList.remove('on');
+  });
+});
+depth2.forEach(function(element) {
+  element.addEventListener('mouseover', () => {
+    const depth2Parent = element.closest('li');
+    depth2Parent.classList.add('on');
+  });
+  element.addEventListener('mouseleave', () => {
+    const depth2Parent = element.closest('li');
+    depth2Parent.classList.remove('on');
   });
 });
 
